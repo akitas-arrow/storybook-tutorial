67c0faf58fe6ed621e76b689a65386f0f7ecca16
akitas-arrow <ayanobayern2@gmail.com>
2025-01-19 13:28:45 +0900

    fix: 背景色を修正 github actionsの動作確認のため

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -30,7 +30,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ backgroundColor: "red" }}
+          style={{ backgroundColor: "pink" }}
         />
       </label>
 
